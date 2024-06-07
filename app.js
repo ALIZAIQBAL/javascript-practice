5ce0013b8f6ed4b0d503f43327091055f41d29dd
ALIZAIQBAL <alizaiqbal322@gmail.com>
2024-06-07 17:21:06 +0500

    practice

---
+++ app.js
@@ -0,0 +1,28 @@
+// for (var i = 1; i <= 5; i++) {
+
+//     for (var j = 1; j <= 5; j++) {
+//         document.write("*")
+
+//     }
+//     document.write("<br>")
+
+// }
+
+
+// for (var i = 1; i <= 5; i++) {
+
+//     for (var j = 1; j <= i; j++) {
+//         document.write("*")
+
+//     }
+//     document.write("<br>")
+
+// }
+
+// for (i=1; i<=5; i++){
+//     for (j=5; j>=i; j--){
+//         document.write("*")
+    
+//     }
+//     document.write("<br>")
+// }
